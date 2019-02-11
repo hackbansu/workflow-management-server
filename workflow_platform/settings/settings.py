df7d4c6cc7d28c578d11a1ff6a3950a9f416a2fa
pawan <pawan.bansal@joshtechnologygroup.com>
2019-02-11 19:44:44 +0530

    Static files serve settings added, requirements file updated

--- workflow_platform/settings/settings.py
+++ workflow_platform/settings/settings.py
@@ -136,7 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/user/'
+STATICFILES_DIRS = ( 'user/', ) 
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
