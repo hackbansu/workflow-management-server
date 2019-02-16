22f819606d05d4b0386257a75dfeb880dfc8cae9
pawan <pawan.bansal@joshtechnologygroup.com>
2019-02-16 16:51:14 +0530

    review fixes

--- apps/company/urls.py
+++ apps/company/urls.py
@@ -7,11 +7,12 @@ from apps.company import views as company_views
 
 router = routers.SimpleRouter()
 router.register('create-company', company_views.CreateCompanyUserView)
-router.register('', company_views.EmployeesView)
+router.register('employees', company_views.EmployeesView)
 router.register('update-company', company_views.UpdateCompanyView)
 router.register('company', company_views.InviteEmployeeView)
 router.register('company', company_views.CreateCompanyView)
 router.register('employee', company_views.EmployeeCompanyView)
+router.register('employee-detail', company_views.RetreiveEmployee)
 
 urlpatterns = router.urls
 
