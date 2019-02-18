f7a20f28ac6287b46d66a512716e112978ee9229
Akhilesh <akhilesh.rao@joshtechnologygroup.com>
2019-02-18 18:11:11 +0530

    admin.py updated for workflow

--- apps/workflow/admin.py
+++ apps/workflow/admin.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-# Register your models here.
+from django.contrib import admin
+
+from apps.workflow.models import Workflow, Task, WorkflowAccess
+
+
+class WorkflowAdmin(admin.ModelAdmin):
+    list_display = ('name', 'template', 'creator', 'start_at', 'complete_at', 'duration')
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = tuple(field.name for field in Task._meta.get_fields())
+
+
+class WorkflowAccessAdmin(admin.ModelAdmin):
+    list_display = tuple(field.name for field in WorkflowAccess._meta.get_fields())
+
+
+admin.site.register(Workflow, WorkflowAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(WorkflowAccess, WorkflowAccessAdmin)
