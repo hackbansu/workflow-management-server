a6e9b69aa7a23d55c5c2ace419d726c1b1bc973d
Akhilesh <akhilesh.rao@joshtechnologygroup.com>
2019-02-26 15:43:32 +0530

    user admin bug fix

--- apps/auth/admin.py
+++ apps/auth/admin.py
@@ -2,14 +2,32 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from apps.auth.models import User
 
 
-class UserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     '''
     UserAdmin to be use with django admin app.
     '''
-    list_display = ('email', 'id', 'first_name', 'last_name')
+    change_user_password_template = None
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                    'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'id', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
