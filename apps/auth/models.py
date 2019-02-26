cffc991bae9e4d6790ed889239d660b140c4bc9b
Akhilesh <akhilesh.rao@joshtechnologygroup.com>
2019-02-26 15:47:32 +0530

    patch blank true for profile

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -95,6 +95,7 @@ class User(AbstractUser):
     )
     profile_photo = models.ImageField(
         upload_to=usr_profil_dir,
+        blank=True,
         default='user/profile/fallback.png',
         help_text='User profile photo'
     )
