5c1a6287d4470cc98112e262d26a54fd38977150
pawan <pawan.bansal@joshtechnologygroup.com>
2019-03-01 06:02:15 +0530

    Report fixes

--- apps/report/serializers.py
+++ apps/report/serializers.py
@@ -5,11 +5,40 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
-from apps.company.serializers import UserCompanySignupSerializer
-from apps.workflow.models import Workflow
-from apps.workflow.serializers import TaskBaseSerializer, WorkflowBaseSerializer
+from apps.company.models import UserCompany
+from apps.workflow.models import Task, Workflow
 
 User = get_user_model()
+
+
+class UserBasicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'email')
+        read_only_fields = ('id', 'first_name', 'last_name', 'email')
+
+
+class EmployeeBasicSerializer(serializers.ModelSerializer):
+    user = UserBasicSerializer()
+
+    class Meta:
+        model = UserCompany
+        fields = ('id', 'user')
+        read_only_fields = ('id', 'user')
+
+
+class TaskBasicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('id', 'title', 'completed_at')
+        read_only_fields = ('id', 'title', 'completed_at')
+
+
+class WorkflowBasicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workflow
+        fields = ('id', 'name', 'completed_at')
+        read_only_fields = ('id', 'name', 'completed_at')
 
 
 class IJLEmployeeCountSerializer(serializers.Serializer):
@@ -18,12 +47,12 @@ class IJLEmployeeCountSerializer(serializers.Serializer):
 
 
 class TopEmployeeSerializer(serializers.Serializer):
-    employee = UserCompanySignupSerializer()
+    employee = EmployeeBasicSerializer()
     avg_task_time = serializers.DurationField()
 
 
 class WorkflowTimeSerializer(serializers.Serializer):
-    workflow = WorkflowBaseSerializer()
+    workflow = WorkflowBasicSerializer()
     total_time_spent = serializers.DurationField()
 
     class Meta:
@@ -35,15 +64,15 @@ class EmployeeReportSerializer(serializers.ModelSerializer):
     number_of_workflows_assigned = serializers.IntegerField(min_value=0)
     number_of_tasks = serializers.IntegerField(min_value=0)
     total_time_spent_on_tasks = serializers.DurationField()
-    Avg_time_spent_on_tasks = serializers.DurationField()
+    avg_time_spent_on_tasks = serializers.DurationField()
     max_time_spent_on_tasks = serializers.DurationField()
     min_time_spent_on_tasks = serializers.DurationField()
     total_time_spent_on_workflows = serializers.DurationField()
     avg_time_spent_on_workflows = serializers.DurationField()
     max_time_spent_on_workflows = serializers.DurationField()
     min_time_spent_on_workflows = serializers.DurationField()
-    last_task_completed = TaskBaseSerializer()
-    last_workflow_completed = WorkflowBaseSerializer()
+    last_task_completed = TaskBasicSerializer()
+    last_workflow_completed = WorkflowBasicSerializer()
     workflows_completed_monthly = IJLEmployeeCountSerializer(many=True)
 
     class Meta:
@@ -51,20 +80,21 @@ class EmployeeReportSerializer(serializers.ModelSerializer):
         fields = ('first_name', 'last_name', 'email',
                   'time_spent_on_workflows',
                   'number_of_workflows_assigned',
-                  'number_of_tasks', 'total_time_spent_on_tasks', 'Avg_time_spent_on_tasks', 'max_time_spent_on_tasks',
+                  'number_of_tasks', 'total_time_spent_on_tasks', 'avg_time_spent_on_tasks', 'max_time_spent_on_tasks',
                   'min_time_spent_on_tasks', 'total_time_spent_on_workflows', 'avg_time_spent_on_workflows',
                   'max_time_spent_on_workflows', 'min_time_spent_on_workflows', 'last_task_completed',
                   'last_workflow_completed', 'workflows_completed_monthly')
 
 
 class WorkflowReportSerializer(serializers.ModelSerializer):
-    unique_assignees = UserCompanySignupSerializer(many=True)
+    unique_assignees = EmployeeBasicSerializer(many=True)
     total_time_spend = serializers.DurationField()
     number_of_assignees = serializers.IntegerField(min_value=0)
     number_of_tasks = serializers.IntegerField(min_value=0)
     average_task_complete_time = serializers.DurationField()
-    assingee_with_min_time = UserCompanySignupSerializer()
-    assingee_with_max_time = UserCompanySignupSerializer()
+    assingee_with_min_time = EmployeeBasicSerializer()
+    assingee_with_max_time = EmployeeBasicSerializer()
+    creator = EmployeeBasicSerializer()
 
     class Meta:
         model = Workflow
